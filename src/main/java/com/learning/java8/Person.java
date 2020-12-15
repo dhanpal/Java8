a2a6a1bb1b2bc01904cd14e47596da5826aaa1f2
dhanpalchauhan <dhanpalchauhan@localhost>
2020-12-15 19:16:40 +0100

    demo

--- src/main/java/com/learning/java8/Person.java
+++ src/main/java/com/learning/java8/Person.java
@@ -1,47 +1,85 @@
 package com.learning.java8;
 
+import java.time.LocalDate;
+import java.time.chrono.IsoChronology;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
 
-	private String firstName;
-
-	private String lastName;
-
-	private String age;
-
-	public Person(String firstName, String lastName, String age) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
+	public enum Sex {
+		MALE, FEMALE
 	}
 
-	public String getFirstName() {
-		return firstName;
+	String name; 
+	LocalDate birthday;
+	Sex gender;
+	String emailAddress;
+
+	Person(String nameArg, LocalDate birthdayArg,
+			Sex genderArg, String emailArg) {
+		name = nameArg;
+		birthday = birthdayArg;
+		gender = genderArg;
+		emailAddress = emailArg;
+	}  
+
+	public int getAge() {
+		return birthday
+				.until(IsoChronology.INSTANCE.dateNow())
+				.getYears();
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void printPerson() {
+		System.out.println(name + ", " + this.getAge());
 	}
 
-	public String getLastName() {
-		return lastName;
+	public Sex getGender() {
+		return gender;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public String getName() {
+		return name;
 	}
 
-	public String getAge() {
-		return age;
+	public String getEmailAddress() {
+		return emailAddress;
 	}
 
-	public void setAge(String age) {
-		this.age = age;
+	public LocalDate getBirthday() {
+		return birthday;
 	}
 
-	@Override
-	public String toString() {
-		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	public static int compareByAge(Person a, Person b) {
+		return a.birthday.compareTo(b.birthday);
+	}
+
+	public static List<Person> createRoster() {
+
+		List<Person> roster = new ArrayList<>();
+		roster.add(
+				new Person(
+						"Fred",
+						IsoChronology.INSTANCE.date(1980, 6, 20),
+						Person.Sex.MALE,
+						"fred@example.com"));
+		roster.add(
+				new Person(
+						"Jane",
+						IsoChronology.INSTANCE.date(1990, 7, 15),
+						Person.Sex.FEMALE, "jane@example.com"));
+		roster.add(
+				new Person(
+						"George",
+						IsoChronology.INSTANCE.date(1991, 8, 13),
+						Person.Sex.MALE, "george@example.com"));
+		roster.add(
+				new Person(
+						"Bob",
+						IsoChronology.INSTANCE.date(2000, 9, 12),
+						Person.Sex.MALE, "bob@example.com"));
+
+		return roster;
 	}
 
 }
