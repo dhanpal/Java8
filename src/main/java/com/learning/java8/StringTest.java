b3f695e6c7058a20c5cb78502a5ee4b30faea576
Dhanpal Chauhan <dhanpalchauhan@Dhanpals-MacBook-Pro.local>
2021-02-24 19:31:54 +0100

    practice

--- src/main/java/com/learning/java8/StringTest.java
+++ src/main/java/com/learning/java8/StringTest.java
@@ -9,6 +9,13 @@ public class StringTest {
 		System.out.println("x -> "+ x);
 		System.out.println("y -> "+ y);
 		
+		String a = "Java";
+		String b = a;
+		a.concat(" bean");
+		System.out.println("a ->"+ a);
+		System.out.println("a ->"+ a.toString());
+		System.out.println("b ->"+ b);
+		System.out.println("a concat ->"+ a.concat(" Bean"));
 	}
 
 }
